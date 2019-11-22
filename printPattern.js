00164d7ac8024c5a311a19e6974104b9e3c73dba
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2019-11-22 11:00:48 +0530

    removed an extra line from main.js

--- printPattern.js
+++ printPattern.js
@@ -1,7 +1,6 @@
 const getShape = require('./src/getShape.js').getShape;
 
 const printPattern = function(){
-
     shapePatternOptions = process.argv.slice(2);
     console.log(getShape(shapePatternOptions));
 };
